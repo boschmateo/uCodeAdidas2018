6277cd08eb8405fd8687d7a88a205d805657d064
Jordi García L <jgarcialisson@gmail.com>
2018-03-10 03:08:49 +0100

    Anadir metodo start()

--- app/src/main/java/com/jordigarcial/ucodeadidas2018/ProductDetailActivity.java
+++ app/src/main/java/com/jordigarcial/ucodeadidas2018/ProductDetailActivity.java
@@ -1,5 +1,7 @@
 package com.jordigarcial.ucodeadidas2018;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +12,12 @@ public class ProductDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
     }
+
+    public static void start(Context context, Product product) {
+        Intent intent = new Intent(context, ProductDetailActivity.class);
+        intent.putExtra(product); //
+        context.startActivity(intent);
+    }
+
+
 }
