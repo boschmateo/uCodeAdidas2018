972b833097171c52952610b9146a01c6072e083b
Roger Bosch <roger.boschm@estudiants.urv.cat>
2018-03-10 17:00:18 +0100

    Now shoes images display in the list

--- app/src/main/java/Adapters/ProductListAdapter.java
+++ app/src/main/java/Adapters/ProductListAdapter.java
@@ -4,9 +4,13 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.net.Uri;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +18,20 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.jordigarcial.ucodeadidas2018.Product;
 import com.jordigarcial.ucodeadidas2018.R;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,7 +45,9 @@ public class ProductListAdapter extends  RecyclerView.Adapter<ProductListAdapter
     Context context;
 
     public ProductListAdapter(List<Product> products, Context context){
+
         this.products=products;
+        this.context=context;
     }
 
     @Override
@@ -50,11 +63,18 @@ public class ProductListAdapter extends  RecyclerView.Adapter<ProductListAdapter
     }
 
     @Override
-    public void onBindViewHolder(PersonViewHolder personViewHolder, int i) {
-        final String path = "res/drawable/" + products.get(i).getId() +".jpg";
+    public void onBindViewHolder(final PersonViewHolder personViewHolder, int i) {
+
         personViewHolder.name.setText(products.get(i).getName());
-        personViewHolder.price.setText(products.get(i).getPrice()+"€");
-        islandref = storageRef.child();
+        personViewHolder.price.setText(products.get(i).getPrice() + "€");
+
+        Resources r = context.getResources();
+        int drawableId = r.getIdentifier(products.get(i).getId(), "drawable", "com.jordigarcial.ucodeadidas2018");
+        //System.out.println("product id: "+products.get(i).getId()+" ID nou: "+drawableId+" ID vell:"+R.drawable.ac7033);
+        personViewHolder.image.setImageResource(drawableId);
+
+    }
+
 
     @Override
     public void onAttachedToRecyclerView(RecyclerView recyclerView) {
