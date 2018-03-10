a1fdffc0696c34ebb97f031727f09b0794af9bd5
Roger Bosch <roger.boschm@estudiants.urv.cat>
2018-03-10 04:06:17 +0100

    Product class added

--- app/src/main/java/com/jordigarcial/ucodeadidas2018/Product.java
+++ app/src/main/java/com/jordigarcial/ucodeadidas2018/Product.java
@@ -1,23 +1,51 @@
 package com.jordigarcial.ucodeadidas2018;
 
-import java.util.List;
-import com.google.firebase.database;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.firebase.database.DataSnapshot;
 
 /**
  * Created by roger on 10/03/2018.
  */
 
-public class Product  {
+public class Product implements Serializable{
 
     private String id;
     private String description;
     private String name;
     private float price;
-    private List<Integer> size;
+    private Map<Integer, Integer> size;
 
-    public Product(){
-
+    public Product(DataSnapshot dataSnapshot){
+        size = new HashMap<>();
+        id = dataSnapshot.getKey();
+        description = (String)dataSnapshot.child("description").getValue();
+        name = (String)dataSnapshot.child("name").getValue();
+        price = (float)dataSnapshot.child("price").getValue();
+        for (DataSnapshot item : dataSnapshot.child("size").getChildren()){
+            size.put(Integer.parseInt(item.getKey()), (Integer) item.getValue());
+        }
     }
 
+    public String getId() {
+        return id;
+    }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public Map<Integer, Integer> getSize() {
+        return size;
+    }
 }
