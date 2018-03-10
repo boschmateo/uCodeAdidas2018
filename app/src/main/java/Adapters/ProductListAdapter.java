c9e54c2c0f9445499d64fa4d48c5f1cd39bb996b
Sergi Quevedo <sergi.quevedo@estudiants.urv.cat>
2018-03-10 04:39:56 +0100

    primera part

--- app/src/main/java/Adapters/ProductListAdapter.java
+++ app/src/main/java/Adapters/ProductListAdapter.java
@@ -5,27 +5,44 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.jordigarcial.ucodeadidas2018.Product;
 import com.jordigarcial.ucodeadidas2018.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by sergi on 10/03/2018.
  */
 
-public class ProductListAdapter {
+public class ProductListAdapter extends ArrayAdapter<Product> {
 
-    protected Activity activity;
+    ArrayList<Product> products;
+    TextView name;
+    TextView price;
+
+    public ProductListAdapter(Context context, ArrayList<Product> product) {
+        super(context,0,product);
+        this.products = product;
+    }
+
     public View getView(int position, View convertView, ViewGroup parent) {
-        
+        Product product = getItem(position);
+
         if(convertView == null) {
-            LayoutInflater li = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = li.inflate(R.layout.product,parent,false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.product,parent,false);
         }
+
+        //Get the two textfields information
+        name = (TextView) convertView.findViewById(R.id.name);
+        price = (TextView) convertView.findViewById(R.id.price);
+        name.setText(product.getName());
+        price.setText(product.getPrice()+"");
         
         return convertView;
     }
 
-    public DataSnapshot hola() {
-
-    }
 }
--- app/src/main/java/com/jordigarcial/ucodeadidas2018/ProductListActivity.java
+++ app/src/main/java/com/jordigarcial/ucodeadidas2018/ProductListActivity.java
@@ -2,17 +2,45 @@ package com.jordigarcial.ucodeadidas2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import Adapters.ProductListAdapter;
 
 public class ProductListActivity extends AppCompatActivity {
 
     ListView listView;
+    ArrayList<Product> product = new ArrayList<Product>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
 
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("products");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                product.add(new Product(dataSnapshot));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
         listView=(ListView) findViewById(R.id.listView);
+        ProductListAdapter adapter = new ProductListAdapter(this,product);
+        listView.setAdapter((ListAdapter) adapter);
 
     }
 }
